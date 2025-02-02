1f9ffad24637fa15773721c75d679de7ab26aa6a
MathiasCPE <93118751+MathiasCPE@users.noreply.github.com>
2025-02-02 19:09:13 +0000

    fix: varchar to text

--- Migrations/20240621154946_InitialCreate.Designer.cs
+++ Migrations/20240621154946_InitialCreate.Designer.cs
@@ -37,7 +37,7 @@ namespace DotNetCoreSqlDb.Migrations
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Description")
-                        .HasColumnType("varchar(max)");
+                        .HasColumnType("text");
 
                     b.HasKey("ID");
 
